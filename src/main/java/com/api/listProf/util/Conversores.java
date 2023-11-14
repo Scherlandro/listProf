62ad96e2e24692953ebc4c1f2ed431357e03f0e6
Scherlandro <scherlandro@gmail.com>
2023-11-14 11:15:07 -0300

    API SIMPLES

--- src/main/java/com/api/listProf/util/Conversores.java
+++ src/main/java/com/api/listProf/util/Conversores.java
@@ -1,48 +1,15 @@
 package com.api.listProf.util;
 
-import com.fasterxml.jackson.databind.util.Converter;
-import org.springframework.boot.autoconfigure.web.format.DateTimeFormatters;
-import org.springframework.core.convert.support.GenericConversionService;
-import org.springframework.data.convert.Jsr310Converters;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.stereotype.Component;
-
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
-import static java.time.OffsetTime.ofInstant;
 
-@Component
-public class Conversores  {
+public class Conversores {
 
+    String pattern = "MMM dd, yyyy HH:mm:ss.SSSSSSSS";
+    String timestampAsString = "Nov 12, 2018 13:02:56.12345678";
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+    LocalDateTime localDateTime = LocalDateTime.from(formatter.parse(timestampAsString));
+    Timestamp timestamp = Timestamp.valueOf(localDateTime);
 }
-
-/*
-    public static long formataStringToLong(String data) {
-
-
-
-        DateTimeFormatter dateTimeFormat =  new DateTimeFormatter(DateTimeFormatter.BASIC_ISO_DATE);
-
-        LocalDateTime date = dateTimeFormat.parse(a, LocalDateTime::from);
-
-        if (data == null || data.equals("")) {
-            return new Long(0);
-        }
-        Calendar calendar = new GregorianCalendar();
-        SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy");
-
-        try {
-            calendar.setTime(sd.parse(data));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return calendar.getTimeInMillis();
-    }
-
-
-}
-*/
