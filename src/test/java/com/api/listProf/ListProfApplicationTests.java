0863b9de835b523bf0bc25f0c4bf436a9fdae3ed
Scherlandro Araújo Carvalho <45185386+Scherlandro@users.noreply.github.com>
2023-11-15 16:39:46 -0300

    Update ListProfApplicationTests.java

--- src/test/java/com/api/listProf/ListProfApplicationTests.java
+++ src/test/java/com/api/listProf/ListProfApplicationTests.java
@@ -12,28 +12,6 @@ class ListProfApplicationTests {
 
 	@Test
 	void contextLoads() {
-/*	List<Profissionais> profList = new ArrayList<>();
-		for (int i = 0; i < profList.size() ; i++) {
-			Profissionais profl = new Profissionais();
-			profl.setId_prof(1);
-			profl.setNome("nomeTeste");
-			profl.setCargo("desenvolvedor");
-			profl.setNascimnento(Timestamp.valueOf("2000-01-10 00:00:00.0"));
-			profl.setCreated_date(Timestamp.valueOf("2023-11-14T01:00:58.544+00:00"));
-		profList.add(profl);
-		}
 
-		List<Contatos> contList = new ArrayList<>();
-		for (int i = 0; i < contList.size() ; i++) {
-			Contatos cont = new Contatos();
-			cont.setId(1);
-			cont.setNome("nomeTeste");
-			cont.setContato("064992642340");
-			cont.setCreated_date(Timestamp.valueOf("2023-11-14T01:00:58.544+00:00"));
-			contList.add(cont);
-		}
-		System.out.println(contList);
-      System.out.println(profList);
-	*/
 	}
 }
